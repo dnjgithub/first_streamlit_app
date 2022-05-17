72ef3d46da1182588261d862be0620e83bf5bc81
Dhananjay Kumar <dnjmgrbgp@gmail.com>
2022-05-17 23:31:44 +0530

    New Scetion to Display Fruityvice api response

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,6 +20,11 @@ fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 
+#New Scetion to Display Fruityvice api response
+improt request
+fruityvice_response =  request.get("https://fruityvice.com/api/fruit/watermelon")
+streamlit.text(fruityvice_response)
+
 
 
    
