3aa725010fd1dce9c0b477ae7f9c9e4fdce384d0
Dhananjay Kumar <dnjmgrbgp@gmail.com>
2022-05-17 23:41:26 +0530

    Update streamlit_app.py
    New Scetion to Display Fruityvice api response

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,6 +27,9 @@ import requests
 fruityvice_response =  requests.get("https://fruityvice.com/api/fruit/watermelon")
 streamlit.text(fruityvice_response.json())
 
+furityvice_normalized = panda.json_normalize(fruityvice_response.json())
+streamlit.dataframe(furityvice_normalized)
+
 
 
    
