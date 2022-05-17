100010bc50db193f826efe33e187dc2580b1306f
Dhananjay Kumar <dnjmgrbgp@gmail.com>
2022-05-17 23:35:20 +0530

    New Scetion to Display Fruityvice api response

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,7 +21,7 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 
 #New Scetion to Display Fruityvice api response
-improt request
+import request
 fruityvice_response =  request.get("https://fruityvice.com/api/fruit/watermelon")
 streamlit.text(fruityvice_response)
 
